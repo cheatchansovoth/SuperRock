d7d47f782609e86a6453b9a8979f2cf8a1a86731
cheatchansovoth <cheatchansovoth@gmail.com>
2022-09-19 22:13:28 -0700

    Update

--- Super_Rock/frontend/src/Pages/Home.js
+++ Super_Rock/frontend/src/Pages/Home.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import {Container,Row,Col} from 'react-bootstrap';
-
+import {Container,Row,Col,Button} from 'react-bootstrap';
+import TredingMenu from './Tredingmenu';
 import './Home.css'
 const Home=()=>
 {
     return (
+        <div>
         <Container className='Container'>
             <Row>
                 <Col lg={6}>
                 <h1>Hungry ?</h1>
-                <h3>Why don't you get food from us ?</h3>
+                <h3 className='mt-4'>Why don't you get food from us ?</h3>
+                <Button className='mt-4'>Order Now!!</Button>
                 </Col>
                 <Col lg={6}>
                     <img src='https://play-lh.googleusercontent.com/2wYuaJlrof7lp_sh-rhZ0FlWSORID5ZTy5JXJcT_GCRvXf-7O6ysFU3Hf4XRCALwlQ=w600-h300-pc0xffffff-pd' alt='burger' style={{width:'480px'}}/>
                 </Col>
             </Row>
+            <Row>
+                <Col>
+                    <TredingMenu/>
+                </Col>
+            </Row>
         </Container>
+        </div>
     )
 }
 
---
+++ Super_Rock/frontend/src/Pages/Tredingmenu.js
@@ -0,0 +1,46 @@
+import {useEffect,useState} from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import './Tredingmenu.css'
+
+
+const Tredingmenu=()=>{
+
+    const [food,setFood]=useState([]);
+
+    useEffect(()=>{
+        getFood();
+    },[]);
+    const getFood= async()=>
+    {
+        const api=await fetch(`https://api.spoonacular.com/recipes/random?apiKey=ea86987b39784e98bea16d848a4994ee&number=9`)
+        const data=await api.json();
+
+       setFood(data.recipes)
+    }
+        return (
+            <div className='display-flex'>
+            <h1 className='mb-3'>Treding Menu</h1>
+            <Splide options={{
+                perPage:4,
+                arrows:false,
+                pagination: false,
+                drag:'free',
+                gap:'5rem'
+            }}>
+                    {food.map((recipe)=>{
+                        return (
+                            <SplideSlide>
+                            <div key={recipe.id} className='Card mt-5'>
+                                <p>{recipe.title}</p>
+                                <img src={recipe.image} alt={recipe.image} className='mt-3'/>
+                            </div>
+                            </SplideSlide>
+                        );
+                    })}
+            </Splide>
+            </div>
+        )
+}
+
+export default Tredingmenu;
