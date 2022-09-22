14854b3d350535e157e2a6cff7e18c24b2d8b3ac
cheatchansovoth <cheatchansovoth@gmail.com>
2022-09-21 20:14:09 -0700

    Update app.js

--- Super_Rock/backend/app.js
+++ Super_Rock/backend/app.js
@@ -3,7 +3,14 @@ const bodyparser= require('body-parser');
 
 const mongoose= require('mongoose');
 const app=express();
+app.use(express.json());
 
+const mongoUrl="mongodb+srv://chansovoth:12345@cluster0.cuai0oi.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoUrl,{
+    useNewUrlParser:true,
+}).then(()=>{
+    console.log('DB is conntected');
+}).catch((e)=>console.log(e));
 const port=5000;
 
 app.get('/',(req,res,next)=>
