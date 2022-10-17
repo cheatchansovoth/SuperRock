a7654893c6fc7a01666ef03a8ae1b5d542a55242
HunchoV <fyn7116@autuni.ac.nz>
2022-10-17 20:41:40 +1300

    Update Payment.js

--- Super_Rock/frontend/src/Pages/Payment.js
+++ Super_Rock/frontend/src/Pages/Payment.js
@@ -12,7 +12,6 @@ import {
     MDBCol,
     MDBCard,
     MDBCardBody,
-    MDBCardImage,
     MDBInput,
     MDBIcon,
   }
@@ -48,26 +47,66 @@ const Payment=()=>
     return (
         <MDBContainer>
         <MDBCard className='text-black m-5' style={{borderRadius: '25px'}}>
-        <h1 className='mt-5 mb-5'>Sign Up</h1>
+        <h1 className='mt-5 mb-5'>Checkout Page</h1>
           <MDBCardBody>
             <form onSubmit={Formik.handleSubmit}>
             <MDBRow>
+
+              {/* Billing Address column */}
               <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
+              <h1 className='mt-5 mb-5'>Billing Address</h1>
                 <div className="d-flex flex-row align-items-center mb-4 ">
                   <MDBIcon fas icon="user me-3" size='lg'/>
-                  <MDBInput label='Email' id='form1' type='text' className={`w-100 ${Formik.touched.name && Formik.errors.name}`} name='Email' value={Formik.values.name} onChange={Formik.handleChange}
+                  <MDBInput label='Full Name' id='form1' type='text' className={`w-100 ${Formik.touched.name && Formik.errors.name}`} name='fName' placeholder='John Smith' value={Formik.values.name} onChange={Formik.handleChange}
                   />
                 </div>
                   {Formik.errors.name &&Formik.touched.name ? <p>{Formik.errors.name}</p>:null}
                 <div className="d-flex flex-row align-items-center mb-4">
                   <MDBIcon fas icon="envelope me-3" size='lg'/>
-                  <MDBInput label='Name On Card' id='form2' type='text' name='CreditHolderName' className={`${Formik.touched.email && Formik.errors.email}`} value={Formik.values.email}  onChange={Formik.handleChange}  
+                  <MDBInput label='Address' id='form2' type='text' name='Address' placeholder='21 Jump Street' className={`${Formik.touched.email && Formik.errors.email}`} value={Formik.values.email}  onChange={Formik.handleChange}  
                   />
                 </div>
                 {Formik.errors.email && Formik.touched.email ? <p>{Formik.errors.email}</p>:null}
                 <div className="d-flex flex-row align-items-center mb-4">
                   <MDBIcon fas icon="lock me-3" size='lg'/>
-                  <MDBInput label='Credit Card Number' id='form3' type='password' name='CreditCardNumber' className={`${Formik.touched.password && Formik.errors.password}`} value={Formik.values.password} onChange={Formik.handleChange}  
+                  <MDBInput label='City' id='form3' type='text' name='City' maxlength='12' placeholder='Auckland' className={`${Formik.touched.password && Formik.errors.password}`} value={Formik.values.password} onChange={Formik.handleChange}  
+                  />
+                </div>
+                {Formik.errors.password && Formik.touched.password ? <p>{Formik.errors.password}</p>:null}
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="key me-3" size='lg'/>
+                  <MDBInput label='Suburb' id='form4' type='text' name='Suburb' placeholder='Parnell' className={`${Formik.touched.confirm && Formik.errors.confirm}`} value={Formik.values.confirm} onChange={Formik.handleChange} 
+                  />
+                </div>
+                {Formik.errors.confirm && Formik.touched.confirm ? <p>{Formik.errors.confirm}</p>:null}
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="key me-3" size='lg'/>
+                  <MDBInput label='Postcode' id='form4' type='text' name='CVC' maxlength='4' placeholder='1234' className={`${Formik.touched.confirm && Formik.errors.confirm}`} value={Formik.values.confirm} onChange={Formik.handleChange} 
+                  />
+                </div>
+                {Formik.errors.confirm && Formik.touched.confirm ? <p>{Formik.errors.confirm}</p>:null}
+  
+              </MDBCol>
+
+              {/* Payment column */}
+  
+              <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex flex-column align-items-center'>
+              <h1 className='mt-5 mb-5'>Payment</h1>
+              <div className="d-flex flex-row align-items-center mb-4 ">
+                  <MDBIcon fas icon="user me-3" size='lg'/>
+                  <MDBInput label='Email' id='form1' type='email' className={`w-100 ${Formik.touched.name && Formik.errors.name}`} name='Email' placeholder='john@gmail.com' value={Formik.values.name} onChange={Formik.handleChange}
+                  />
+                </div>
+                  {Formik.errors.name &&Formik.touched.name ? <p>{Formik.errors.name}</p>:null}
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="envelope me-3" size='lg'/>
+                  <MDBInput label='Name On Card' id='form2' type='text' name='CreditHolderName' placeholder='John Smith' className={`${Formik.touched.email && Formik.errors.email}`} value={Formik.values.email}  onChange={Formik.handleChange}  
+                  />
+                </div>
+                {Formik.errors.email && Formik.touched.email ? <p>{Formik.errors.email}</p>:null}
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="lock me-3" size='lg'/>
+                  <MDBInput label='Credit Card Number' id='form3' type='password' name='CreditCardNumber' maxlength='12' placeholder="1111-2222-3333-4444" className={`${Formik.touched.password && Formik.errors.password}`} value={Formik.values.password} onChange={Formik.handleChange}  
                   />
                 </div>
                 {Formik.errors.password && Formik.touched.password ? <p>{Formik.errors.password}</p>:null}
@@ -79,16 +118,25 @@ const Payment=()=>
                 {Formik.errors.confirm && Formik.touched.confirm ? <p>{Formik.errors.confirm}</p>:null}
                 <div className="d-flex flex-row align-items-center mb-4">
                   <MDBIcon fas icon="key me-3" size='lg'/>
-                  <MDBInput label='CVC' id='form4' type='password' name='CVC' className={`${Formik.touched.confirm && Formik.errors.confirm}`} value={Formik.values.confirm} onChange={Formik.handleChange} 
+                  <MDBInput label='CVC' id='form4' type='password' name='CVC' maxlength='3' className={`${Formik.touched.confirm && Formik.errors.confirm}`} value={Formik.values.confirm} onChange={Formik.handleChange} 
                   />
                 </div>
                 {Formik.errors.confirm && Formik.touched.confirm ? <p>{Formik.errors.confirm}</p>:null}
                 <MDBBtn className='mb-4' size='lg' type="submit">Confirm</MDBBtn>
-  
+                
               </MDBCol>
-  
-              <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
-                <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid/>
+
+              {/* Cart column */}
+
+              <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex flex-column align-items-center'>
+              <h1 className='mt-5 mb-5'>Cart</h1>
+              <div className="d-flex flex-row align-items-center mb-4 ">
+                  <MDBIcon fas icon="user me-3" size='lg'/>
+                  <MDBInput label='Email' id='form1' type='email' className={`w-100 ${Formik.touched.name && Formik.errors.name}`} name='Email' placeholder='john@gmail.com' value={Formik.values.name} onChange={Formik.handleChange}
+                  />
+                </div>
+                  {Formik.errors.name &&Formik.touched.name ? <p>{Formik.errors.name}</p>:null}
+                
               </MDBCol>
   
             </MDBRow>
