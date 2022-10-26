5c0942664db1d77be3617c41f58811ab5e17c484
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2022-10-26 04:51:59 -0700

    about.js
    responsive

--- Super_Rock/backend/client/src/Pages/About.js
+++ Super_Rock/backend/client/src/Pages/About.js
@@ -10,11 +10,11 @@ const About=()=>
         <div className='about-section'>
             <Container className='mt-5'>
                 <Row>
-                    <Col lg={6}>
+                    <Col lg={6} sm={12}>
                     <img src={photo} alt="" className='about-image'/>
                     </Col>
-                    <Col lg={4} className='m-5 '>
-                        <div className='mt-5'>
+                    <Col lg={4} sm={12}>
+                    <div className='mt-5 text-center'>
                     <h1 className='text-center fw-normal'>ABOUT US</h1>
                     <p>All of our menu items are inspired by SiChuan cuisine and have been created by our head chef Kenny after studying authentic SiChuan cuisine in China. Not only do we have fresh flown-in seafood from the northeast,
                     but we also have a variety of handcrafted cocktails, wine, and beer to choose from.</p>
