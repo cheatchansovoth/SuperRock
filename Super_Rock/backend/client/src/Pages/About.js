cc1fc6db263f682ab262c7a257ec0665628a3233
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2022-10-26 04:41:18 -0700

    about updated
    correct size and center div

--- Super_Rock/backend/client/src/Pages/About.js
+++ Super_Rock/backend/client/src/Pages/About.js
@@ -7,7 +7,7 @@ const About=()=>
 {
 
     return (
-        <div>
+        <div className='about-section'>
             <Container className='mt-5'>
                 <Row>
                     <Col lg={6}>
@@ -15,9 +15,9 @@ const About=()=>
                     </Col>
                     <Col lg={4} className='m-5 '>
                         <div className='mt-5'>
-                    <h1>ABOUT US</h1>
-                      All of our menu items are inspired by SiChuan cuisine and have been created by our head chef Kenny after studying authentic SiChuan cuisine in China. Not only do we have fresh flown-in seafood from the northeast,
-                    but we also have a variety of handcrafted cocktails, wine, and beer to choose from.
+                    <h1 className='text-center fw-normal'>ABOUT US</h1>
+                    <p>All of our menu items are inspired by SiChuan cuisine and have been created by our head chef Kenny after studying authentic SiChuan cuisine in China. Not only do we have fresh flown-in seafood from the northeast,
+                    but we also have a variety of handcrafted cocktails, wine, and beer to choose from.</p>
                     </div>
                     </Col>
                 </Row>
